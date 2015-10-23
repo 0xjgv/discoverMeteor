8cd5510d432ad76dd164f59b049725c72c9a58c5
juansgaitan <juans.gaitan@gmail.com>
2015-10-23 10:32:47 -0500

    Added edit posts form.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,15 +6,17 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
     };
+    console.log(post.url);
 
     Meteor.call('postInsert', post, function (error, result) {
       // display the error to the user and abort
-      if (error) return alert(error.reason);
+      if (error) 
+        return alert(error.reason);
 
       // show this result but route anyway
       if (result.postExists)
         alert('This link has already been posted');
-      
+
       Router.go('postPage');
     });
   }
